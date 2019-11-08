c2cb7656abc1473fbe2a22398d3872a70a23d980
SayedHamidTabatabaei <43670355+SayedHamidTabatabaei@users.noreply.github.com>
2019-11-08 12:28:46 +0330

    add swagger

--- CodeGeneratorService/Controllers/ProcedureController.cs
+++ CodeGeneratorService/Controllers/ProcedureController.cs
@@ -1,18 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CodeGenerator.Share.Enums;
-using Microsoft.AspNetCore.Http;
+﻿using CodeGenerator.Share.Enums;
+using CodeGeneratorService.Builders.ParameterModel;
+using CodeGeneratorService.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
 namespace CodeGeneratorService.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/[controller]s")]
     [ApiController]
     public class ProcedureController : ControllerBase
     {
+        #region Fields
+
+        private readonly IGeneratorBusiness _generatorBusiness;
+
+        #endregion
+
+        #region constructor 
+
+        public ProcedureController(IGeneratorBusiness generatorBusiness)
+        {
+            _generatorBusiness = generatorBusiness;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Get all procedures for input table name
+        /// </summary>
+        /// <param name="tableNames">Table name that we want procedures</param>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<string> Get(List<string> tableNames)
         {
             return "";
@@ -21,7 +44,9 @@ namespace CodeGeneratorService.Controllers
         [HttpGet]
         public ActionResult<string> Get(List<string> tableNames, ActionCategories actionCategories)
         {
-            return "";
+            var sqlProcedureParameterModel = new SqlProcedureParameterModel { ActionCategory = actionCategories, };
+
+            return _generatorBusiness.Get(sqlProcedureParameterModel, actionCategories);
         }
 
     }
--- CodeGeneratorService/Startup.cs
+++ CodeGeneratorService/Startup.cs
@@ -1,9 +1,15 @@
-﻿using CodeGeneratorService.Extensions;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using CodeGeneratorService.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace CodeGeneratorService
 {
@@ -21,14 +27,68 @@ namespace CodeGeneratorService
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.AddApiVersioning(o => o.ApiVersionReader = new HeaderApiVersionReader("api-version"));
+
             services.RegisterServices();
             services.RegisterBuilder();
             services.RegisterBodyBuilder();
+
+            #region Swagger
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                //c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Code generator Api", Version = "v1" });
+
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Code generator Api",
+                    Description = "Generate scripts and c# code service",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    //Contact = new OpenApiContact
+                    //{
+                    //    Name = "Shayne Boyer",
+                    //    Email = string.Empty,
+                    //    Url = new Uri("https://twitter.com/spboyer"),
+                    //},
+                    //License = new OpenApiLicense
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = new Uri("https://example.com/license"),
+                    //}
+                });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First()); //This line
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            #region Swagger
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodeGenerator API V1");
+            });
+
+            #endregion
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
